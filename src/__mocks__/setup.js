c4992be18dd4b792508e99fdcc7448ddf7165e2b
Nitin <nitinkumar.piparava@chronext.com>
2022-08-03 14:27:52 +0200

    Mocking some of node modules

---
+++ src/__mocks__/setup.js
@@ -0,0 +1,5 @@
+import 'react-native-gesture-handler/jestSetup';
+require('react-native-reanimated/lib/reanimated2/jestUtils').setUpTests();
+
+//Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
